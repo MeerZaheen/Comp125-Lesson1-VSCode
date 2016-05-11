17812a573c4d2879c5bd8eb7f6c6186aef038f00
MeerZaheen <MeerZ@Meers-MacBook-Pro.local>
2016-05-11 13:52:59 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Comp125-Lesson1
+# Comp125-Lesson1
---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+/* Custom JavaScript */
+// self-executing anonymous function -> know for quiz
+// IIFE - Immediately Invoked Functions Expression -> know for quiz
+(function(){
+    console.log("App Started...");
+    
+    // Variable Declaration And Initialization
+    
+    var number = 0;
+    number = Math.floor((Math.random() * 10) + 1);
+    console.log(number);
+})();
